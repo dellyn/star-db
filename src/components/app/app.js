b636f9b1f9caa65db32951f9035d95055b413fb1
Igor Makarchuk <ingvardellyn@gmail.com>
2020-10-09 20:55:46 +0300

    initial commit

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./.css";
+
+export default class RandomPlanet extends Component {
+  render() {
+    return (
+      <div className>
+        <div></div>
+      </div>
+    );
+  }
+}
---
+++ src/components/item-list/list-item.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./list-item.css";
+
+export default class ListItem extends Component {
+  render() {
+    return (
+      <div className>
+        <div></div>
+      </div>
+    );
+  }
+}
